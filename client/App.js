adbd7bbb0ef3e897477e56aa3eff26f73b56f6b7
Eduardo Trujillo <edytr1997@gmail.com>
2018-08-26 00:01:06 -0500

    Add views to stack navigator

--- client/App.js
+++ client/App.js
@@ -4,10 +4,14 @@ import { createStackNavigator } from 'react-navigation';
 
 import Main from './app/components/main';
 import Playlist from './app/components/playlist';
+import Create from './app/components/create'
+import Search from './app/components/search'
 
 export default createStackNavigator({
   Main: Main,
-  Playlist: Playlist
+  Playlist: Playlist,
+  Create: Create,
+  Search: Search
 }, {
   initialRouteName: 'Main'
 });

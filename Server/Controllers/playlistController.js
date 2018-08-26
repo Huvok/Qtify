161ea94a6541b4daf0262f060fdecd3722948250
Hugo Garcia <hugo_os_95@hotmail.com>
2018-08-25 19:05:04 -0500

    Finished voting and adding.

--- Server/Controllers/playlistController.js
+++ Server/Controllers/playlistController.js
@@ -31,28 +31,19 @@ module.exports = {
             collaborative: true
         }, config)
         .then(function (response) {
-            let query = 'INSERT INTO groups (group_name, token) VALUES (?, ?);';
-            conn.query(query, [playlistName, authToken], function (err, results, fields) {
+            let query = 'INSERT INTO groups (id, group_name, token) VALUES (?, ?, ?);';
+            conn.query(query, [response.data['id'], playlistName, authToken], function (err, results, fields) {
                 if (err)
                     console.log(err);
                 else {
-                    // TODO: Check if this query always works.
-                    query = 'SELECT id FROM groups WHERE group_name = ? AND token = ?';
-                    conn.query(query, [playlistName, authToken], function (err, results, fields) {
+                    query = 'INSERT INTO users_groups VALUES (?, ?);';
+                    conn.query(query, [userId, response.data['id']], function (err, results, fields) {
                         if (err)
                             console.log(err);
-                        else {
-                            // In theory there must be exactly one result (no more and not less),
-                            // that is why we use the element at position 0 within the results arr.
-                            let groupId = results[0].id; 
-                            query = 'INSERT INTO users_groups VALUES (?, ?);';
-                            conn.query(query, [userId, groupId], function (err, results, fields) {
-                                if (err)
-                                    console.log(err);
-                                else
-                                    res.send(JSON.stringify(groupId));
-                            });
-                        }
+                        else
+                            res.send(JSON.stringify({
+                                response: 'OK'
+                            }));
                     });
                 }
             });

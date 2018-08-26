2d10d31ea1ffda69fe91ef9c4097dc879b8ea927
Eduardo Trujillo <edytr1997@gmail.com>
2018-08-25 21:24:43 -0500

    Implement search

--- client/app/components/search.js
+++ client/app/components/search.js
@@ -1,12 +1,75 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList, ActivityIndicator } from 'react-native';
 
-export class Search extends React.Component {
+import { searchSongs } from '../model/spotify';
+
+export default class Search extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      isLoading: false,
+      songs: []
+    };
+  }
+
+  searchSong(song) {
+    let token = 'BQBeOYJGQHTRjBbgq6oAiOM7rFT8y5UOu8z6MUu3lzKreQNZCw_Gca_SkbytRHzjNxtkKE_HX1CdsCfd39Spmkqyap17YPC8D00xV4qr2XREX1TABuOTywctKWe41j2Ch3VtHs7NLeoY9ZgpWc5CKHNhiJi-FdRdCg2e0qsRDb8BgcAdtw9vfkcBytzJ--2h-IHpRKdje5-mVu96o8K4-cjcsgQYOQ0oMpoy2Ea6BBAtEb6i0dZAZeo0vQj_sBXv8NPNcDSBXmRywWoWHncOsK6Dd9t_j4n6atVo';
+
+    this.setState({
+      text: song,
+      isLoading: true,
+      songs: []
+    });
+
+    searchSongs(song, token).then((result) => {
+      this.setState({
+        text: song,
+        isLoading: false,
+        songs: result
+      });
+    });
+  }
+
   render() {
-    return (
-      <View>
-        <Text>Search for a playlist!</Text>
-      </View>
-    );
+    if(this.state.isLoading) {
+      return (
+        <View>
+          <TextInput
+            value={this.state.text}
+            placeholder="Search for a song"
+            onChangeText={(text) => this.searchSong(text)}
+          />
+          <ActivityIndicator />
+        </View>
+      );
+    } else if(this.state.songs.length == 0) {
+      return (
+        <View>
+        <TextInput
+        value={this.state.text}
+        placeholder="Search for a song"
+        onChangeText={(text) => this.searchSong(text)}
+        />
+        <Text>There are no results</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View>
+        <TextInput
+        value={this.state.text}
+        placeholder="Search for a song"
+        onChangeText={(text) => this.searchSong(text)}
+        />
+        <FlatList
+          data={this.state.songs}
+          renderItem={({item}) => <Text>{item.name}, {item.artist}</Text>}
+          keyExtractor={(item, index) => index.toString()}
+        />
+        </View>
+      );
+    }
   }
 }
